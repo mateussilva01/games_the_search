917b28f1bf752545515c7ac3a7f9a9c75fac6814
mateussilva01 <mateussilva.dev01@gmail.com>
2024-03-26 19:18:20 -0300

    Refatoração da função de formatação de data.

--- src/js/data_utilities.js
+++ src/js/data_utilities.js
@@ -1,5 +1,5 @@
-const changeMainTitleColor = () => mainTitle.style.color = "#2BBBAD";
-const changeReleasedTitleColor = () => releasedTitle.style.color = "#2BBBAD";
+const changeMainTitleColor = () => mainTitle.style.color = '#2BBBAD';
+const changeReleasedTitleColor = () => releasedTitle.style.color = '#2BBBAD';
 
 const handleErrorImageNull = (image) => {
   image.onerror = '';
@@ -11,18 +11,7 @@ const checkValueIfNullOrZero = value =>  value === 0 ? 'Nenhuma' : value ?? 'Nen
 
 const addZeroInDate = number => number <= 9 ? number = `0${number}` : number;
 
-const formatDate = (element) => {
-  let data = new Date(element);
-  let day = (data.getDate().toString());
-  let month = ((data.getMonth()+1).toString());
-  let year = (data.getFullYear());
-  let getFormatDate = (addZeroInDate(day) + "/" + (addZeroInDate(month)) + "/" + year);
-  let showFormatDate = getFormatDate.slice(0, 10);
-
-  const result = !showFormatDate ? '' : showFormatDate;
-
-  return result;
-}
+const formatDate = (date) => date ? new Date(date).toLocaleDateString('pt-BR') : new Date().toLocaleDateString('pt-BR');
 
 const limitText = (text) => {
   const limit = 80;
